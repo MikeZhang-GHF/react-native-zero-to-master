63d1c5e9c9c69bb21833cb4c7221aa3239043486
Mike Zhang <zhixiang.zhang2011@gmail.com>
2023-05-20 11:59:50 -0600

    Baseline list

--- README.md
+++ README.md
@@ -13,3 +13,42 @@ expo init myapp
 > View, Text, Image, Button, TextInput, ScrollView, StyleSheet, FlatList, SectionList, ActivityIndicator, Alert, Modal, TouchableHighlight, TouchableOpacity, TouchableNativeFeedback, TouchableWithoutFeedback, Switch, Picker, Slider, StatusBar, WebView
 > View like <div> in HTML, holding other components
 > Text like <p> in HTML, holding text
+
+#### App Template
+> StyleSheet.create() to create a stylesheet object to hold styles for components
+
+```javascript
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
+
+export default function App() {
+	return (
+    <View style={styles.AppContainer}>
+      <View>
+        <TextInput placeholder="Your course goal!" />
+        <Button title="Add Goal" />
+      </View>
+      <View>
+        <Text>List of goals...</Text>
+      </View>
+    </View>
+	);
+}
+
+const styles = StyleSheet.create({
+	AppContainer: {
+    padding: 50,
+	},
+});
+```
+
+#### Flexbox
+> - Layouts are created with Flexbox
+> - Flexbox is designed to provide a consistent layout on different screen sizes
+> - Main Axis and Cross Axis
+
+- View is a flexbox container by default
+> View as container used as a layout tool, it should be used as much as possible to wrap other components
+
+- FlatList
+> FlatList is better than ScrollView when dealing with a list of data 
--- myapp/App.js
+++ myapp/App.js
@@ -1,27 +1,92 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { useState } from 'react';
+import {
+	StyleSheet,
+	Text,
+	View,
+	Button,
+	TextInput,
+	FlatList,
+} from 'react-native';
 
 export default function App() {
+	// State
+	const [enteredGoalText, setEnteredGoalText] = useState('');
+	const [courseGoals, setCourseGoals] = useState([]);
+
+	// Event handlers
+	const goalInputHandler = (enteredText) => {
+		setEnteredGoalText(enteredText);
+	};
+
+	const addGoalHandler = () => {
+		setCourseGoals((currentGoals) => [
+			...currentGoals, // spread operator
+			{ text: enteredGoalText, id: Math.random().toString() },
+		]);
+	};
+
 	return (
-		<View style={styles.container}>
-			<Text style={styles.dummyText}> Another piece of text!</Text>
-			<Text style={styles.dummyText}>Hello World!</Text>
-			<Button title="Tap me!" />
+		<View style={styles.appContainer}>
+			<View style={styles.inputContainer}>
+				<TextInput
+					style={styles.textInput}
+					placeholder="Your course goal!"
+					onChangeText={goalInputHandler}
+				/>
+				<Button title="Add Goal" onPress={addGoalHandler} />
+			</View>
+			<View style={styles.goalsContainer}>
+				<FlatList
+					data={courseGoals}
+					renderItem={(itemData) => {
+						return (
+							<View style={styles.goalItem}>
+								<Text style={styles.goalText}>
+									{itemData.item.text}
+								</Text>
+							</View>
+						);
+					}}
+					alwaysBounceVertical={false}
+          keyExtractor={(item, index) => item.id}
+				/>
+			</View>
 		</View>
 	);
 }
 
 const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		backgroundColor: '#fff',
-		alignItems: 'center',
-		justifyContent: 'center',
+	appContainer: {
+		flex: 1, // takes up all available space
+		padding: 50,
+		paddingHorizontal: 16,
 	},
-	dummyText: {
-		margin: 16,
-		borderWidth: 2,
-		borderColor: 'red',
-		padding: 16,
+	inputContainer: {
+		flex: 1,
+		flexDirection: 'row',
+		justifyContent: 'space-between',
+		alignItems: 'center',
+		marginBottom: 24,
+		borderBottomWidth: 1,
+		borderBottomColor: '#cccccc',
+	},
+	textInput: {
+		borderWidth: 1,
+		borderColor: '#cccccc',
+		width: '70%',
+		marginRight: 8,
+		padding: 8,
+	},
+	goalsContainer: {
+		flex: 5,
+	},
+	goalItem: {
+		margin: 8,
+		padding: 8,
+		boarderRadius: 8,
+		backgroundColor: '#5e0acc',
+	},
+	goalText: {
+		color: 'white',
 	},
 });
